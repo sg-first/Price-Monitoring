aade886ddb8ad0861777c5eebad9a8cbc2fb6fd0
Mingkai Zhou <524019351@qq.com>
2024-11-09 13:05:18 +0800

    修复服务器上运行不了的bug

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver # 从selenium库里引入驱动器模块，用于控制浏览器。
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By # 定位需要用到by模块，譬如by name或者by ID或者byXpath
 from selenium.webdriver.support.ui import WebDriverWait # 显示等待模块，用于提取动态网站的元素。
 from selenium.webdriver.support import expected_conditions as EC#
@@ -10,7 +11,7 @@ from email.mime.text import MIMEText
 from email.utils import formataddr
 
 # 蛇形排列处理
-prelist = [4.7919,4.7876,4.7972]+[4.8300]*140+[4.8200]*140+[4.8400]*45+[4.8100]*44+[4.8481,4.8507,4.8482,4.8478,4.8490,4.8486,4.8471,4.8475]
+prelist = [4.65400,4.66100,4.6630]+[4.6700]*140+[4.6800]*140+[4.7200]*45+[4.7100]*44+[4.777,4.750,4.762,4.7633,4.499,4.493,4.521,4.511]
 sorted_prelist = sorted(prelist)
 manuallist = []
 for i in range(len(prelist)//2):
@@ -24,8 +25,8 @@ def judge_ifnot_del():
 def sendemail(price_list,current_price):
     sorted_list = price_list[:]
     sorted_list.sort()
-    percentnum = sorted_list[6]
-    percent = 6 / len(sorted_list)*100
+    percentnum = sorted_list[115]
+    percent = 115 / len(sorted_list)*100
     if current_price <= percentnum:
         msg_from = 'xxxx@163.com'
         password = 'xxxxx'
@@ -44,12 +45,11 @@ def sendemail(price_list,current_price):
         except BaseException as e:
             print(e)
 
-
 def getdata():
     global manuallist
     # 环境变量添加了的话就不用手打路径。启动driver
-    driver = webdriver.edge(
-        executable_path=r"C:\Program Files\Python312\Scripts\edgedriver_win64\msedgedriver.exe")
+    path = Service(r"C:\Program Files\Python312\Scripts\edgedriver_win64\msedgedriver.exe")
+    driver = webdriver.Edge(service=path)
     driver.get("https://gushitong.baidu.com/foreign/global-AUDCNY")
     # 先设置个等待时间，20s加载到这个元素。指定的等待条件是找到这个元素，定位器是一个包括定位方法和定位值的元组。使用父级元素定位。
     element = WebDriverWait(driver, 20).until(
@@ -61,7 +61,6 @@ def getdata():
     sendemail(manuallist,data)
     driver.quit()
     return data
-
 
 def run_schedule():
     while True:
@@ -76,4 +75,4 @@ schedule.every(15).minutes.do(getdata)
 
 while True:
     print(manuallist)
-    time.sleep(10)
+    time.sleep(20)
